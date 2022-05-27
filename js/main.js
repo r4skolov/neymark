1e12813073f25775dfecf5c2d606f2cdb85a0000
Artem Davydchenkov <davydchenkov1@gmail.com>
2022-05-27 23:54:27 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("тут пишешь код");
+  const swiper = new Swiper('.merch__swiper', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
 
 });
