54478ec9bf3aaef189b3566393020f482d6f94dc
Artem Davydchenkov <davydchenkov1@gmail.com>
2022-05-28 21:51:44 +0300

    update

--- js/main.js
+++ js/main.js
@@ -13,4 +13,42 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   });
 
+  const scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 300
+  });
+  // if (window.screen.width > 1200) {
+  //   gsap.set(".content:not(.initial)", { autoAlpha: 0 });
+
+  //   var headlines = gsap.utils.toArray(".text");
+  //   var totalDuration = 1500;
+
+
+  //   ScrollTrigger.create({
+  //     trigger: ".pin-up",
+  //     start: "top top",
+  //     end: "+=" + totalDuration,
+  //     pin: true,
+  //     scrub: true,
+  //     animation: lineTimeline,
+  //   });
+
+
+  //   var target = document.querySelectorAll(".test");
+  //   var target2 = document.querySelectorAll(".metaverse__item");
+  //   var observer = new MutationObserver(function (mutations) {
+  //     mutations.forEach(function (mutationRecord) {
+  //       target2.forEach((elem, i) => {
+  //         elem.classList.remove("active");
+  //       });
+  //       mutationRecord.target
+  //         .closest(".metaverse__item")
+  //         .classList.add("active");
+  //     });
+  //   });
+
+  //   target.forEach((elem, i) => {
+  //     observer.observe(elem, { attributes: true, attributeFilter: ["style"] });
+  //   });
+  // }
+
 });
