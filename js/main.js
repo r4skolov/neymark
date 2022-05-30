de2e9b6a881cdab39c0cd05a15878c1fb0bcc3c2
Artem Davydchenkov <davydchenkov1@gmail.com>
2022-05-30 22:03:22 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", () => {
+  //slider merch
   const swiper = new Swiper('.merch__swiper', {
     slidesPerView: 1,
     spaceBetween: 0,
@@ -13,42 +14,133 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   });
 
+  // scroll к секции
   const scroll = new SmoothScroll('a[href*="#"]', {
     speed: 300
   });
-  // if (window.screen.width > 1200) {
-  //   gsap.set(".content:not(.initial)", { autoAlpha: 0 });
-
-  //   var headlines = gsap.utils.toArray(".text");
-  //   var totalDuration = 1500;
 
 
-  //   ScrollTrigger.create({
-  //     trigger: ".pin-up",
-  //     start: "top top",
-  //     end: "+=" + totalDuration,
-  //     pin: true,
-  //     scrub: true,
-  //     animation: lineTimeline,
-  //   });
+  // запуск видео по скролу в секции
+  window.addEventListener('load', videoScroll);
+  window.addEventListener('scroll', videoScroll);
+  
+  function videoScroll() {
+  
+    
+      var windowHeight = window.innerHeight,
+          videoEl = document.querySelectorAll('.video-block__item');
+  
+      for (var i = 0; i < videoEl.length; i++) {
+  
+        var thisVideoEl = videoEl[i],
+            videoHeight = thisVideoEl.clientHeight,
+            videoClientRect = thisVideoEl.getBoundingClientRect().top;
+  
+        if ( videoClientRect <= ( (windowHeight) - (videoHeight*.5) ) && videoClientRect >= ( 0 - ( videoHeight*.5 ) ) ) {
+          thisVideoEl.play();
+        } else {
+          thisVideoEl.pause();
+        }
+  
+      }
+    
+  
+  }
 
 
-  //   var target = document.querySelectorAll(".test");
-  //   var target2 = document.querySelectorAll(".metaverse__item");
-  //   var observer = new MutationObserver(function (mutations) {
-  //     mutations.forEach(function (mutationRecord) {
-  //       target2.forEach((elem, i) => {
-  //         elem.classList.remove("active");
-  //       });
-  //       mutationRecord.target
-  //         .closest(".metaverse__item")
-  //         .classList.add("active");
-  //     });
-  //   });
 
-  //   target.forEach((elem, i) => {
-  //     observer.observe(elem, { attributes: true, attributeFilter: ["style"] });
-  //   });
-  // }
+  // бургер меню
+  function disableScroll() {
+    let pagePosition = window.scrollY;
+    document.body.classList.add('scroll');
+    document.body.dataset.position = pagePosition;
+    document.body.style.top = -pagePosition + 'px';
+  }
+  
+  function enableScroll() {
+    let pagePosition = parseInt(document.body.dataset.position, 10);
+    document.body.style.top = 'auto';
+    document.body.classList.remove('scroll');
+    window.scroll({
+      top: pagePosition,
+      left: 0
+    });
+    document.body.removeAttribute('data-position');
+  }
+  
+  
+  const burger = document.querySelector('.burger');
+  const menu = document.querySelector('.nav');
+  const menuWrap = document.querySelector('.nav__wrapper');
+  const menuItem = document.querySelectorAll('.nav__link');
+  burger.addEventListener('click', () => {
+    menu.classList.toggle('overlay');
+    burger.classList.toggle('burger--active');
+    menuWrap.classList.toggle('nav--active');
+
+    
+    if (burger.classList.contains('burger--active')) {
+      disableScroll();
+    } else {
+      enableScroll();
+    }
+  });
+
+  menuItem.forEach(el => {
+    el.addEventListener('click', () => {
+      menu.classList.remove('overlay');
+      burger.classList.remove('burger--active');
+      menuWrap.classList.remove('nav--active');
+      enableScroll();
+    })
+  })
+  
+
+
+  // точки на карте
+  const dotsBtn = document.querySelectorAll('.map__dots');
+  const dotsItem = document.querySelectorAll('.locations__item');
+
+  dotsBtn.forEach(function(item) {
+      item.addEventListener("click", function() {
+
+          let currentBtn = item;
+          let dotId = currentBtn.getAttribute("data-dot"); 
+          let currentDot = document.querySelector(dotId); 
+
+
+          if (! currentBtn.classList.contains('active') ) {
+          dotsBtn.forEach (function(item) {
+              item.classList.remove('active');
+          });
+
+          currentBtn.classList.add('active');
+
+          dotsItem.forEach(function(item){
+              item.classList.remove('active');
+          });
+
+          currentDot.classList.add('active');
+          }
+      });
+  });
+
+ //скролл в секции metaverse 
+	var controller = new ScrollMagic.Controller({globalSceneOptions: {
+    duration: 250,
+  }});
+
+	new ScrollMagic.Scene({triggerElement: "#sec1"})
+					.setClassToggle("#high1", "active")
+					.addTo(controller);
+	new ScrollMagic.Scene({triggerElement: "#sec2"})
+					.setClassToggle("#high2", "active") 
+					.addTo(controller);
+	new ScrollMagic.Scene({triggerElement: "#sec3"})
+					.setClassToggle("#high3", "active") 
+					.addTo(controller);
+	new ScrollMagic.Scene({triggerElement: "#sec4"})
+					.setClassToggle("#high4", "active")
+					.addTo(controller);
 
 });
